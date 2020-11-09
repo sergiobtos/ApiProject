be17d1ca0d666187adf1c81266d1b4b01d54a8ea
Sergio Silva <sergiobtos@hotmail.com>
2020-11-09 17:47:38 -0500

    V0
    some alterations models and Data

---
+++ Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Data
+{
+    public interface IRepository
+    {
+        //GetAll/getById/post/put/delete method
+
+        IEnumerable<Employee> GetAll();
+
+    }
+}
